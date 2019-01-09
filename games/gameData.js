50a1859764cd0d115093f38d2fdea94a6fec6e16
seattleowl <seattleowl@users.noreply.github.com>
2019-01-08 16:09:19 -0800

    fix 3 for main and games sites.

--- games/gameData.js
+++ games/gameData.js
@@ -1,16 +1,16 @@
 var gameData = [
-    // js / gs
-    {name: "Matching Game", language: "gs", url: "http://seattleowl.com/games/gameSquare/matchingGame"},
-    {name: "Shooter", language: "gs", url: "http://seattleowl.com/games/gameSquare/shooter"},
-    {name: "Basic GS Platformer", language: "gs", url: "http://seattleowl.com/games/gameSquare/platformer"},
+    // js / GameSquare
+    {name: "Matching Game", language: "GameSquare", url: "http://seattleowl.com/games/gameSquare/matchingGame"},
+    {name: "Shooter", language: "GameSquare", url: "http://seattleowl.com/games/gameSquare/shooter"},
+    {name: "Basic GS Platformer", language: "GameSquare", url: "http://seattleowl.com/games/gameSquare/platformer"},
     {name: "Basic Platformer", language: "js", url: "http://seattleowl.com/games/gameSquare/matchingGame"},
 
     // scratch
-    {name: "You Need... A Mocha.", language: "scratch", url: "https://scratch.mit.edu/projects/276669361/"},
-    {name: "Platformer Engine", language: "scratch", url: "https://scratch.mit.edu/projects/228094654/"},
-    {name: "Walls", language: "scratch", url: "https://scratch.mit.edu/projects/208404973/"},
-    {name: "Octi-simulation", language: "scratch", url: "https://scratch.mit.edu/projects/268090301/"},
-    {name: "Jasoka National Park", language: "scratch", url: "https://scratch.mit.edu/projects/218423385/"},
+    {name: "You Need... A Mocha.", language: "Scratch", url: "https://scratch.mit.edu/projects/276669361/"},
+    {name: "Platformer Engine", language: "Scratch", url: "https://scratch.mit.edu/projects/228094654/"},
+    {name: "Walls", language: "Scratch", url: "https://scratch.mit.edu/projects/208404973/"},
+    {name: "Octi-simulation", language: "Scratch", url: "https://scratch.mit.edu/projects/268090301/"},
+    {name: "Jasoka National Park", language: "Scratch", url: "https://scratch.mit.edu/projects/218423385/"},
 
     // 
 ]
