1280e87b49388f931c6d7c5fa77edb90a25492ef
seattleowl <32573897+seattleowl@users.noreply.github.com>
2020-06-29 16:21:36 -0700

    Remove CMS

--- src/main.js
+++ src/main.js
@@ -1,48 +1,78 @@
-let lastY = window.scrollY
-let header = document.getElementById("header")
+let lastY = window.scrollY;
+let header = document.getElementById("header");
 
 window.onscroll = () => {
-    if (window.scrollY > Math.min(lastY, 130)) {
-        header.classList.add("minimized")
-    } else if (window.scrollY < 130) {
-        header.classList.remove("minimized")
-    }
+	if (window.scrollY > Math.min(lastY, 130)) {
+		header.classList.add("minimized");
+	} else if (window.scrollY < 130) {
+		header.classList.remove("minimized");
+	}
+};
+
+class HTMLPostElement extends HTMLElement {
+	constructor() {
+		super();
+
+		let shadow = this.attachShadow({ mode: "open" });
+
+		let div = document.createElement("div");
+		div.classList.add("post");
+		shadow.appendChild(div);
+
+		let title = document.createElement("h2");
+		title.innerHTML = this.postTitle;
+		div.appendChild(title);
+
+		let img = document.createElement("img");
+		img.src = `images/posts/${this.postImage}.png`;
+		div.appendChild(img);
+
+		let style = document.createElement("style");
+		style.innerHTML = `.post {
+			height: max-content;
+			width: 580px;
+			background-color: var(--color-purple-2);
+			border-radius: 5px;
+			margin-bottom: 10px;
+			padding-bottom: 10px;
+			box-shadow: 0 5px 3px transparent;
+			transition: box-shadow 200ms;
+			text-align: start;
+			cursor: pointer;
+		}
+		
+		.post:hover {
+			box-shadow: 0 5px 3px var(--color-gray-2);
+		}
+		
+		.post h2 {
+			color: var(--color-gray-1);
+			padding-left: 20px;
+			width: 100%;
+		}
+		
+		.post img {
+			width: 580px;
+			border-radius: 0 0 5px 5px;
+		}`;
+		shadow.appendChild(style);
+
+		div.onclick = () => {
+			location.href = this.postUrl;
+		};
+	}
+
+	get postTitle() {
+		return this.getAttribute("post-title");
+	}
+
+	get postImage() {
+		return this.getAttribute("post-img");
+	}
+
+	get postUrl() {
+		return this.getAttribute("post-url");
+	}
 }
 
-let catagory = new URLSearchParams(location.search).get("cat")
-
-ajax("https://seattleowl-cms.herokuapp.com/content").then((data) => {
-    let app = document.getElementById("app")
-
-    if (catagory != "games") data.posts.forEach(post => {
-        let postEl = document.createElement("div")
-        postEl.onclick = () => { location.href = "./post.html?p=" + post.title.toLowerCase().replace(/ /g, "-").replace(/\./g, "-").replace(/!/g, "") }
-        postEl.classList.add("post")
-
-        let header = document.createElement("h2")
-        header.innerHTML = post.title
-        postEl.appendChild(header)
-
-        let img = document.createElement("img")
-        img.src = post.image
-        postEl.appendChild(img)
-
-        app.appendChild(postEl)
-    })
-
-    if (catagory != "posts") data.games.forEach(game => {
-        let postEl = document.createElement("div")
-        postEl.onclick = () => {location.href = game.url}
-        postEl.classList.add("post")
-
-        let header = document.createElement("h2")
-        header.innerHTML = game.title
-        postEl.appendChild(header)
-
-        let img = document.createElement("img")
-        img.src = game.image
-        postEl.appendChild(img)
-
-        app.appendChild(postEl)
-    })
-})
+customElements.define("web-post", HTMLPostElement);
--- src/post.js
+++
@@ -1,7 +0,0 @@
-const md = markdownit()
-let postID = new URLSearchParams(location.search).get("p")
-
-ajax("https://seattleowl-cms.herokuapp.com/post/" + postID, "text").then((data) => {
-    let result = md.render(data)
-    document.getElementById("app").innerHTML += result
-})
